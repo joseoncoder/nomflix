a80c1704b2048fdc2a2c14e27c8cf4be839d926f
M'sWorld <m@MsWorldui-MacBook-Pro.local>
2020-02-13 11:36:25 +0900

    Updates

---
+++ precache-manifest.965e4417af6c8060504b4d94027764af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6891f6f87c34899697999ebcfa640772",
+    "url": "/nomflix/index.html"
+  },
+  {
+    "revision": "e550cdc76b3536629632",
+    "url": "/nomflix/static/js/2.cfb81249.chunk.js"
+  },
+  {
+    "revision": "280b48dbc3281f342da16f66e41cf46c",
+    "url": "/nomflix/static/js/2.cfb81249.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a52a05d277c09198877",
+    "url": "/nomflix/static/js/main.96b85fd5.chunk.js"
+  },
+  {
+    "revision": "e3a2b87533eb2e0897e1",
+    "url": "/nomflix/static/js/runtime-main.a658bcd6.js"
+  },
+  {
+    "revision": "4330f1468a2e63a1b432bdd69c80d16e",
+    "url": "/nomflix/static/media/joseon.4330f146.png"
+  }
+]);
